1a3ba610b08cdcf6deb630a34b45d48a7c1acda1
gao-shan <sam.mosca@outlook.com>
2019-12-30 15:10:07 -0500

    added comments to UserDao explaining how to implement spring boot

--- java/com/revature/repositories/UserDao.java
+++ java/com/revature/repositories/UserDao.java
@@ -1,7 +1,8 @@
 package com.revature.repositories;
 
 import com.revature.models.User;
-
+//must extend JPArepo, so it is not using spring data; takes in type of dao, and type of id of dao
+//can use @Query for complicated queries
 public interface UserDao {
 	public User findUserByUsernameAndPassword(String username, String password);
 }
