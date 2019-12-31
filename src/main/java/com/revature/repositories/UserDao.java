dc263520b1dd8566f7faa44db864ddb3767a1cfb
Justin Massey <jdm9six@gmail.com>
2019-12-31 14:32:01 -0500

    Working login

--- src/main/java/com/revature/repositories/UserDao.java
+++ src/main/java/com/revature/repositories/UserDao.java
@@ -1,11 +1,10 @@
 package com.revature.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.revature.models.User;
 
 public interface UserDao extends JpaRepository<User, Integer>{
-	@Query("FROM User")
+	//@Query("FROM User")
 	public User findUserByUsernameAndPassword(String username, String password);
 }
