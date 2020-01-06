eb35bbcee4e462fbfdd1c6afd673349e78990874
Justin Massey <jdm9six@gmail.com>
2020-01-05 20:34:17 -0500

    Added cors

--- src/main/java/com/revature/configuration/CorsConfig.java
+++ src/main/java/com/revature/configuration/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer{
 	@Override//this is cors for an application level
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // all endpoints in the api
-            .allowedOrigins("http://localhost:1234", "http://localhost:3000") // allow this origin
+            .allowedOrigins("http://top-chat.s3-website-us-east-1.amazonaws.com/", "http://localhost:3000") // allow this origin
             .allowedMethods("PUT", "DELETE", "GET", "OPTIONS", "POST", "PATCH")
             .allowedHeaders("header1", "Content-Type", "Authorization")
             //.exposedHeaders("header1", "header2")
